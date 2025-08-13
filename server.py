a59d9004393d7eb58d6e55db39249725880eefb2
Ben Meredith <ben.meredith1@icloud.com>
2025-08-12 23:39:59 -0400

    Update server.py

--- server.py
+++ server.py
@@ -62,8 +62,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)  # debug=True optional for local testing
